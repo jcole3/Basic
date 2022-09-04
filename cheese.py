cce99b9c6fb422fc48e915ca9dc137633e132179
jcole3 <99101085+jcole3@users.noreply.github.com>
2022-09-03 19:53:53 -0500

    Add files via upload

---
+++ cheese.py
@@ -0,0 +1,5 @@
+import requests
+x = requests.get('https://api-optimistic.etherscan.io/api?module=account&action=tokenbalance&contractaddress=0x7F5c764cBc14f9669B88837ca1490cCa17c31607&address=0x8492be1aed52F19677a59934886e4FeCEF57Cde8&tag=latest&apikey=YourApiKeyToken')
+print(x)
+import requests
+x = requests.get('https://api-optimistic.etherscan.io/api?module=account&action=tokenbalance&contractaddress=0x7F5c764cBc14f9669B88837ca1490cCa17c31607&address=0x8492be1aed52F19677a59934886e4FeCEF57Cde8&tag=latest&apikey=YourApiKeyToken')
